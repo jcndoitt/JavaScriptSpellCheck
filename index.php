3d58a99950e3bd10d5003bf0e896769196b60429
jcndoitt <johnng@doitt.nyc.gov>
2017-04-14 12:25:36 -0400

    Added CORS header

--- index.php
+++ index.php
@@ -1,8 +1,12 @@
 <html>
 <?php
     header("Access-Control-Allow-Origin: *");
-    header("Access-Control-Allow-Methods: GET, POST");
-    ?>
+    header("Access-Control-Allow-Credentials: true");
+    header("Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS");
+    header("Access-Control-Max-Age: 86400");
+    header("Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token , Authorization");
+    header("Access-Control-Allow-Headers: X-Requested-With");
+?>
 <head>
 <title>JavaScriptSpellCheck - Hello World</title>
 <script type='text/javascript' src='https://nnycmspellcheck.herokuapp.com/JavaScriptSpellCheck/include.js' ></script>
