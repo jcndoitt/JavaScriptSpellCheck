9d55bb268b065ba1f0988071205a71745a00efc3
jcndoitt <johnng@doitt.nyc.gov>
2017-04-14 16:25:17 -0400

    modified index.php

--- index.php
+++ index.php
@@ -6,14 +6,22 @@
 $Spelling.DefaultDictionary = 'English (USA)'
 $Spelling.SpellCheckAsYouType('myTextArea')
 </script>
+
+<script type='text/javascript'>
+function onclick() {
+
+    $Spelling.SpellCheckInWindow('myTextArea');
+}
+</script>
+
 </head>
 
 <body>
 
 <textarea name="myTextArea"  id="myTextArea" cols="50" rows="20">Hello Worlb. This Example show JavaScript Spellcheck "as-you-Type" and also spellchecking window functionality on the same page.  Implementation is easy using only a few lines of Java Script - and will work on alost all modern web hosting packages - both Linux and Windows based. </textarea>
 
-<iframe src="" name="myFrame" frameborder="0"></iframe>
-<p><a href="parent.$Spelling.SpellCheckInWindow('myTextArea')" target="myFrame">Spell Check</a></p>
+<iframe src="https://nnycmspellcheck.herokuapp.com/JavaScriptSpellCheck" name="myFrame" frameborder="0"></iframe>
+<p><a href="onclick()" target="myFrame">Spell Check</a></p>
 
 </body>
 </html>
